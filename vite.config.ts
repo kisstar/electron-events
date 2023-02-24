f24a6f59f638533085fe97dfb33e768eaae43d0e
kisstar <dwh.chn@foxmail.com>
2023-02-24 23:07:49 +0800

    build: add an alias for the file system path

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,47 @@
+import { resolve as pResolve } from 'path';
 import { defineConfig } from 'vite';
 import electron from 'vite-plugin-electron';
 import vue from '@vitejs/plugin-vue';
 
+const resolve = (...paths: string[]) => pResolve(__dirname, ...paths);
+
 export default defineConfig(({ command }) => {
   const isDevelopment = command === 'serve';
-  const electronOptions = [
-    {
-      entry: isDevelopment
-        ? 'packages/demo/main/index.ts'
-        : 'packages/core/index.ts'
+  const resolveConfig = {
+    alias: {
+      '@core': resolve('packages/core'),
+      '@demo': resolve('packages/demo')
     }
-  ];
-
-  if (isDevelopment) {
-    electronOptions.push({
-      entry: {
-        preload: 'packages/demo/preload/index.ts'
-      },
-      onstart(options: any) {
-        options.reload();
-      }
-    } as any);
-  }
+  };
 
   return {
-    plugins: [vue(), electron(electronOptions)]
+    resolve: resolveConfig,
+    plugins: [
+      vue(),
+      electron(
+        [
+          {
+            entry: isDevelopment
+              ? 'packages/demo/main/index.ts'
+              : 'packages/core/index.ts',
+            vite: {
+              resolve: resolveConfig
+            }
+          },
+          isDevelopment &&
+            ({
+              entry: {
+                preload: 'packages/demo/preload/index.ts'
+              },
+              onstart(options: any) {
+                options.reload();
+              },
+              vite: {
+                resolve: resolveConfig
+              }
+            } as any)
+        ].filter(Boolean)
+      )
+    ]
   };
 });
