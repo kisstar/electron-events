e1188f0ef3b66600a5cff21dedc375a7b0db137e
Kisstar <dwh.chn@foxmail.com>
2024-01-16 20:36:20 +0800

    fix: error in cjs mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,69 +1,86 @@
 import { resolve as stlResolve } from 'path';
 import { existsSync, rmSync } from 'fs';
-import { defineConfig } from 'vite';
+import { defineConfig, type BuildOptions } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import electron, { Configuration } from 'vite-plugin-electron';
+import electron from 'vite-plugin-electron/simple';
 import pkg from './package.json';
 
 const resolve = (...paths: string[]) => stlResolve(__dirname, ...paths);
-
-existsSync(resolve('dist')) && rmSync(resolve('dist'), { recursive: true });
-existsSync(resolve('dist-electron')) &&
-  rmSync(resolve('dist-electron'), { recursive: true });
+const clean = () => {
+  existsSync(resolve('dist')) && rmSync(resolve('dist'), { recursive: true });
+  existsSync(resolve('dist-electron')) &&
+    rmSync(resolve('dist-electron'), { recursive: true });
+};
+const resolveConfig = {
+  alias: {
+    '@core': resolve('packages/core'),
+    '@demo': resolve('packages/demo'),
+    lodash: 'lodash-es'
+  }
+};
+const notSupportEsModules = ['lodash-es'];
+const cjsBuildConf: BuildOptions = {
+  rollupOptions: {
+    external: [
+      'electron',
+      'events',
+      ...Object.keys(pkg.dependencies || {})
+    ].filter(item => !notSupportEsModules.includes(item))
+  },
+  lib: {
+    entry: 'packages/core/index.ts',
+    formats: ['cjs'],
+    fileName: 'index'
+  },
+  outDir: 'dist-electron'
+};
+const esBuildConf: BuildOptions = {
+  rollupOptions: {
+    external: ['electron', 'events', ...Object.keys(pkg.dependencies || {})]
+  },
+  lib: {
+    entry: 'packages/core/index.ts',
+    formats: ['es'],
+    fileName: 'index'
+  },
+  outDir: 'dist-electron',
+  minify: false
+};
 
 export default defineConfig(({ command }) => {
-  const isDevelopment = command === 'serve';
+  const isBuild = command === 'build';
   const isTest = process.env.NODE_ENV === 'test';
+  const isRealBuild = isBuild && !isTest;
 
-  const resolveConfig = {
-    alias: {
-      '@core': resolve('packages/core'),
-      '@demo': resolve('packages/demo'),
-      lodash: 'lodash-es'
-    }
-  };
-  const electronOptions = [
-    {
-      entry:
-        isDevelopment || isTest
-          ? 'packages/demo/main/index.ts'
-          : 'packages/core/index.ts',
+  if (isRealBuild) {
+    clean();
+  }
+
+  const electronOptions = {
+    main: {
+      entry: 'packages/demo/main/index.ts',
       vite: {
         resolve: resolveConfig,
-        build:
-          isDevelopment || isTest
-            ? undefined
-            : {
-                rollupOptions: {
-                  external: [
-                    'electron',
-                    'events',
-                    ...Object.keys(pkg.dependencies || {})
-                  ]
-                },
-                lib: {
-                  entry: 'packages/core/index.ts',
-                  formats: ['es'],
-                  fileName: 'index'
-                },
-                outDir: 'dist-electron',
-                minify: false
-              }
+        build: isRealBuild ? cjsBuildConf : {}
       }
     },
-    (isDevelopment || isTest) && {
-      entry: {
-        preload: 'packages/demo/preload/index.ts'
-      },
-      onstart: (options: any) => options.reload(),
-      vite: {
-        resolve: resolveConfig
-      }
-    }
-  ].filter(Boolean) as Configuration;
+    preload: isRealBuild
+      ? undefined
+      : {
+          input: {
+            preload: 'packages/demo/preload/index.ts'
+          },
+          vite: {
+            resolve: resolveConfig
+          }
+        }
+  };
 
   return {
     resolve: resolveConfig,
-    plugins: [vue(), electron(electronOptions)]
+    build: {
+      ...(isRealBuild ? esBuildConf : {})
+    },
+    plugins: [!isRealBuild && vue(), electron(electronOptions)].filter(Boolean)
   };
 });
